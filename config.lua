3925da94dd84a60a7b871e3ba854f0afcf74eed1
Swichas <46385597+Swichas@users.noreply.github.com>
2025-07-19 12:02:58 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,13 @@
 cfg = {}
 
-cfg.locale = 'lt' -- Options: 'en', 'lt'
+cfg.locale = 'en' -- Options: 'en', 'lt'
 cfg.framework = 'qbcore' -- Options: 'esx', 'qbcore'
-cfg.menu = "lation_ui" -- Options: 'ox_lib', "lation_ui"
-cfg.notifys = "lation_ui" -- Options: 'ox_lib', "esx", "qbcore", "lation_ui"
+cfg.menu = "ox_lib" -- Options: 'ox_lib', "lation_ui"
+cfg.notifys = "ox_lib" -- Options: 'ox_lib', "esx", "qbcore", "lation_ui"
 
 -- Do you use command or export to open the menu?
 cfg.usecommand = true -- Use command to open the menu
-cfg.command = "darbozinute" -- Command to open the menu
+cfg.command = "jobmessage" -- Command to open the menu
 cfg.useexport = false -- Use export to open the menu, if true, command will be ignored
 cfg.export = "jobmessages:openMenu" -- Export to open the menu
 
@@ -32,7 +32,7 @@ cfg.job = {
 
     },
     ['mechanic'] = {
-        label = 'Mechanikai',
+        label = 'Mechanic',
         color = '#e35c5c', -- collor for the mechanic job
         bossgrade = {'boss'}, -- Boss grade for the police job
         onlyboss = true, -- Only bosses can use the menu
